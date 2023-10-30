8294021b251ebf469e666f3cc738fba297973162
cAPRIcaT <arpitdvd2k3@outlook.com>
2023-10-30 16:03:06 +0530

    python with workflow

---
+++ src/name_in_comment.py
@@ -0,0 +1,8 @@
+import sys
+
+output_handle = open("output.txt", "w+")
+output_handle.write('''My name is Apprentice LM, not PR reviewer :( that's so lame!!!''')
+output_handle.close()
+
+sys.exit(0)
+
