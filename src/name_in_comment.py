77b1fd445cfb1ba0113910698f3165807b01a239
cAPRIcaT <arpitdvd2k3@outlook.com>
2023-10-31 11:22:11 +0530

    updating workflow and source file
    deleted extra yml files, renamed the commenting yml file and updated the comment message

--- src/name_in_comment.py
+++ src/name_in_comment.py
@@ -1,7 +1,7 @@
 import sys
 
 output_handle = open("output.txt", "w+")
-output_handle.write('''My name is Apprentice LM, not PR reviewer :( that's so lame!!!''')
+output_handle.write('''My name is Apprentice LM, and the comment is working''')
 output_handle.close()
 
 sys.exit(0)
