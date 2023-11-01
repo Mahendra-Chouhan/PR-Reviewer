92fd0d0dffe317d848624529620f6f4b34b70a0b
NIMIT <96406498+Nimitzzz@users.noreply.github.com>
2023-11-01 14:33:39 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -1,7 +1,25 @@
-import sys
 
-output_handle = open("src/files/output.txt", "w+")
-output_handle.write('''My name is Apprentice LM, and the comment is working''')
+from transformers import AutoModel, AutoTokenizer
+import glob
+
+checkpoint = "Salesforce/codet5p-220m-bimodal"
+device = "cuda"  # for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
+model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
+
+files = glob.glob("*.py")
+
+for file in files:
+    with open(file, "r") as f:
+        code =f.read()
+
+input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
+
+generated_ids = model.generate(input_ids, max_length=20)
+xyz= print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+# Convert a string of SVG data to an image.
+output_handle.write(xyz)
 output_handle.close()
 
 sys.exit(0)
