5e5394f3c66ad759732e5a974ab277432e5d94a7
Mahendra <mahendrachouhan01@gmail.com>
2023-11-01 12:45:14 +0530

    PYRE added

--- src/commenter.py
+++ src/commenter.py
@@ -1,6 +1,6 @@
 import sys
 
-output_handle = open("output.txt", "w+")
+output_handle = open("src/files/output.txt", "w+")
 output_handle.write('''My name is Apprentice LM, and the comment is working''')
 output_handle.close()
 
