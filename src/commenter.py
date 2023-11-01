e51c23a6d92b47e7850c2e3f7efdfc4630c43bc2
NIMIT <nimitj2003@gmail.com>
2023-11-01 16:59:33 +0530

    code var updated

--- src/commenter.py
+++ src/commenter.py
@@ -10,10 +10,23 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
 model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
 
-files = glob.glob("*.py")
-for file in files:
-    with open(file, "r") as f:
-        code =f.read()
+# files = glob.glob("*.py")
+# for file in files:
+#     with open(file, "r") as f:
+#         code =f.read()
+code = """def svg_to_image(string, size=None):
+    if isinstance(string, unicode):
+        string = string.encode('utf-8')
+        renderer = QtSvg.QSvgRenderer(QtCore.QByteArray(string))
+    if not renderer.isValid():
+        raise ValueError('Invalid SVG data.')
+    if size is None:
+        size = renderer.defaultSize()
+        image = QtGui.QImage(size, QtGui.QImage.Format_ARGB32)
+        painter = QtGui.QPainter(image)
+        renderer.render(painter)
+    return image"""
+
 
 input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
 
