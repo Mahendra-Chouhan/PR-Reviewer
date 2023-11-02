0356a58d47e2c8a199d229f1b98c2549a49b28fd
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-02 15:45:32 +0530

    updated commenter.py
    replaced xyz var with tokens
    made the output handle write the the tokens as soon as they're generated

--- src/commenter.py
+++ src/commenter.py
@@ -19,18 +19,16 @@ files = glob.glob("*.py")
 files = [file for file in files if file != os.path.basename(__file__)]
 print(files)
 output = ""
+output_handle = open("src/files/output.txt", "w+")
 # Read each .py file and print its contents
 for py_file in files:
     with open(py_file, 'r') as f:
         code = f.read()
         input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
         generated_ids = model.generate(input_ids, max_length=20)
-        xyz= tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-        print(xyz)
-        output += xyz
-print(output)
-output_handle = open("src/files/output.txt", "w+")
-output_handle.write(f'{output}')
+        tokens= tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        output_handle.write(f'{tokens}')
+
 output_handle.close()
 sys.exit(0)
 
