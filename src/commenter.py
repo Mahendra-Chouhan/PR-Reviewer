b0f76a2bd24eb07a311d04f844dc8a86cd59cae7
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-07 14:37:58 +0530

    included the write in the for loop

--- src/commenter.py
+++ src/commenter.py
@@ -11,21 +11,21 @@ model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
 
 code = []
 
+# Empty the contents of output.txt
+with open("src/files/output.txt", "w") as f:
+  f.write("")
+
 files = glob.glob("*.py")
 for file in files:
   with open(file, "r") as f:
-    code.append(f.read())
+    code_string = f.read()
 
-# Convert the list of strings to a single string
-code_string = "\n".join(code)
+    input_ids = tokenizer(code_string, return_tensors="pt").input_ids.to(device)
 
-input_ids = tokenizer(code_string, return_tensors="pt").input_ids.to(device)
+    generated_ids = model.generate(input_ids, max_length=20)
+    tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
-generated_ids = model.generate(input_ids, max_length=20)
-tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-
-output_handle = open("src/files/output.txt", "w+")
-output_handle.write(f'{tokens}')
-output_handle.close()
+    with open("src/files/output.txt", "a") as f:
+      f.write(f'{tokens}\n')
 
 sys.exit(0)
