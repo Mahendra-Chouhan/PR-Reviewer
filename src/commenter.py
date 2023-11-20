0d148f103525a3f4dc3bacca3037ed844e39abad
NIMIT <96406498+Nimitzzz@users.noreply.github.com>
2023-11-20 13:46:05 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -1,46 +1,20 @@
-import glob
-import os
-import torch
-from transformers import AutoModel, AutoTokenizer
+model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
+model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin" # the model is in bin format
 
-checkpoint = "Salesforce/codet5p-220m-bimodal"
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from huggingface_hub import hf_hub_download
+from llama_cpp import Llama
+model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
-model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
+lcpp_llm = None
+lcpp_llm = Llama(
+    model_path=model_path,
+    n_threads=2, # CPU cores
+    n_batch=512, # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+    n_gpu_layers=32 # Change this value based on your model and your GPU VRAM pool.
+    )
 
-# Get the path to the src directory
-src_path = os.path.join(os.getenv("GITHUB_WORKSPACE"), "src")
-src_path = os.path.expandvars(src_path)
-print(src_path)
-
-# Find all files in the src directory
-all_files = glob.glob(os.path.join(src_path, "*"))
-print(all_files)
-
-# Loop through all files in the src directory
-for file in all_files:
-
-  # Check if the file is a regular file
-  if os.path.isfile(file):
-
-    # Print the file name
-    print(f"Processing file: {file}")
-
-    # Open the file and read the code
-    with open(file, "r") as f:
-      code = f.read()
-
-    # Encode the code into input IDs
-    input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
-
-    # Generate a comment for the code
-    generated_ids = model.generate(input_ids, max_length=20)
-    tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-
-    # Print the comment
-    print(f"Generated comment: {tokens}")
-
-    # Write the comment to the output file
-    with open("src/files/output.txt", "a") as f:
-      f.write(f"{tokens}\n")
+response=lcpp_llm(prompt=prompt_template, max_tokens=256, temperature=0.5, top_p=0.95,
+                  repeat_penalty=1.2, top_k=150,
+                  echo=True)
+print(response)
+print(response["choices"][0]["text"])
