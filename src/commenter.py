281ceb35b6f44748d3c0b53cab55e90989645373
NIMIT <96406498+Nimitzzz@users.noreply.github.com>
2023-11-21 11:12:02 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -3,9 +3,8 @@ import os
 import torch
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
-
 model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
-model_basename = "llama-2-13b-chat.ggml-vic13b-uncensored-q5_1.bin" # the model is in bin format
+model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
 
 
 # Get the path to the src directory
