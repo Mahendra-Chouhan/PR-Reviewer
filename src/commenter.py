d2a09d64a745300b73f6d4498402a8d7724e5d03
NIMIT <96406498+Nimitzzz@users.noreply.github.com>
2023-11-21 12:12:03 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -37,15 +37,12 @@ for file in all_files:
       code = f.read()
 
     prompt = ("Suggest helpful changes to the code: function additionFunction(a, b) {  return a + b; } let num1 = 5; let num2 = 10; let sum = additionFunction(num1, num2); console.log('Sum of given numbers is :', sum);")
-    prompt_template=f'''SYSTEM: You are a helpful, respectful and honest assistant. Always answer as helpfully.
-
-    USER: {prompt}
-
+    prompt_template=f'''
     ASSISTANT:
     '''
     
     response=lcpp_llm(prompt=prompt_template, max_tokens=256, temperature=0.5, top_p=0.95, repeat_penalty=1.2, top_k=150, echo=True)
-    response = response["choices"][0]["text"][2]
+    response = response["choices"][0]["text"]
 
 
     # Write the comment to the output file
