7ea0a37890ecfabe572d76accf753465124851dc
NIMIT <96406498+Nimitzzz@users.noreply.github.com>
2023-11-21 12:20:05 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -38,6 +38,9 @@ for file in all_files:
 
     prompt = ("Suggest helpful changes to the code: function additionFunction(a, b) {  return a + b; } let num1 = 5; let num2 = 10; let sum = additionFunction(num1, num2); console.log('Sum of given numbers is :', sum);")
     prompt_template=f'''
+
+    USER: {prompt}
+    
     ASSISTANT:
     '''
     
