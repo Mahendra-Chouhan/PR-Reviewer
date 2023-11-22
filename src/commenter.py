faec91b3d621018426679b2417a41fda06d9d8f7
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-22 11:48:07 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -19,10 +19,10 @@ lcpp_llm = Llama(
     n_ctx = 1024
     )
 
-with open("./mydiff.txt", "r") as diff_handle:
+with open("{{github.workspace}}/.github/workflows/mydiff.txt", "r") as diff_handle:
   diff = diff_handle.read()
 
-prompt = ("Suggest helpful changes to the code: \n" + diff)
+prompt = ("Review the code difference and suggest changes: \n" + diff)
 prompt_template=f'''SYSTEM: You are a helpful, respectful and honest assistant. Always answer as helpfully.
 
 USER: {prompt}
