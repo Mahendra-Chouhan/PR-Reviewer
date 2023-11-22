a2b05dff7c25a6279fbba2a82057015eb3b6d1cf
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-22 15:08:15 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -6,8 +6,8 @@ from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
 
-model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
-model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
+model_name_or_path = "TheBloke/Llama-2-7B-GGML"
+model_basename = "llama-2-7b.ggmlv3.q5_K_M.bin"
 
 model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
 # GPU
