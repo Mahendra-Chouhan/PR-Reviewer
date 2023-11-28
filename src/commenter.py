22ec68d04e8feb78cec6d94d9db03674c96f4909
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-28 11:53:54 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -26,11 +26,10 @@ with open(f"{github_workspace_path}/difference_hunk.txt", "r") as diff_handle:
 
 prompt = ("""you are a code review assistant. Concisely summarize the major code difference in ONE LINE, explaining the difference in a way humans can understand. do it in the format:
 
-CHANGE1: Explanation
-CHANGE2 (if any) : Explanation
+CHANGE(s): Explanation
 
 Here is the code difference: """ + diff)
-prompt_template=f'''SYSTEM: You are a helpful, respectful and honest assistant. Always answer as helpfully. 
+prompt_template=f'''SYSTEM: You are a helpful code reviewer.
 
 USER: {prompt}
 
