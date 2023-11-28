e3fe7e8a6f18a94201a78231d432fd70b71bdcc0
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-28 11:56:42 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -24,7 +24,7 @@ github_workspace_path = os.getenv("GITHUB_WORKSPACE")
 with open(f"{github_workspace_path}/difference_hunk.txt", "r") as diff_handle:
     diff = diff_handle.read()
 
-prompt = ("""you are a code review assistant. Concisely summarize the major code difference in ONE LINE, explaining the difference in a way humans can understand. do it in the format:
+prompt = ("""you are a code review assistant. Concisely summarize the major code differences, each in ONE LINE, explaining the difference in a way humans can understand. do it in the format:
 
 CHANGE(s): Explanation
 
