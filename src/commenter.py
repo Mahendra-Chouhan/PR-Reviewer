d900d9dc39e0b802383ebc642e05eb92eb5a5b5d
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-30 13:15:21 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -1,23 +1,5 @@
-import glob
-import os
 import torch
-import re
-from huggingface_hub import hf_hub_download
-from llama_cpp import Llama
-
-
-model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
-model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
-
-model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
-# GPU
-lcpp_llm = Llama(
-    model_path=model_path,
-    n_threads=2, # CPU cores
-    n_batch=512, # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
-    n_gpu_layers=32, # Change this value based on your model and your GPU VRAM pool.
-    n_ctx = 3072
-    )
+from transformers import RobertaTokenizer, RobertaConfig, RobertaModel
 
 # Get the path of the GitHub workspace
 github_workspace_path = os.getenv("GITHUB_WORKSPACE")
@@ -26,20 +8,7 @@ github_workspace_path = os.getenv("GITHUB_WORKSPACE")
 with open(f"{github_workspace_path}/difference_hunk.txt", "r") as diff_handle:
     diff = diff_handle.read()
 
-prompt = ("""you are a code review assistant. Concisely summarize the major code difference in ONE LINE, explaining the difference in a way humans can understand. do it in the format:
 
-CHANGE: Explanation.
-
-Here is the code difference: """ + diff)
-prompt_template=f'''SYSTEM: You are a helpful, respectful and honest assistant. Always answer as helpfully. 
-
-USER: {prompt}
-
-ASSISTANT:
-'''
-    
-response=lcpp_llm(prompt=prompt_template, max_tokens=1536, temperature=0.5, top_p=0.95, repeat_penalty=1.2, top_k=50, echo=False)
-response = response["choices"][0]["text"]
 
 # Write the comment to the output file
 with open("src/files/output.txt", "a") as f:
