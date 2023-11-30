0a9200c5ce50eef9611e8b768f4772bc243ac344
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-30 13:40:11 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -8,6 +8,17 @@ github_workspace_path = os.getenv("GITHUB_WORKSPACE")
 with open(f"{github_workspace_path}/difference_hunk.txt", "r") as diff_handle:
     diff = diff_handle.read()
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = RobertaModel.from_pretrained("microsoft/codebert-base") 
+tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+model.to(device)
+
+#tokenize the code
+tokens = tokenizer.encode(diff, return_tensors = "pt")
+
+#Run CodeBERT on the tokenized code
+with torch.no_grad():
+    response = model(tokens) 
 
 
 # Write the comment to the output file
