d6b41f9df41e9c73911f90e6776ad6b5e30885e0
Mahendra S Chouhan <mahendrachouhan01@gmail.com>
2023-12-05 13:07:18 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -5,7 +5,7 @@ import re
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
-
+#model
 model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
 model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
 
